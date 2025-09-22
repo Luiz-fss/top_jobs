e99573764b65f9f76e26407e355d8adde6e492fd
Luiz-fss <luiz.fss@hotmail.com>
2025-09-22 03:59:46 -0300

    Global Theme and first screen

---
+++ lib/features/auth/ui/screens/splash_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ColoredBox(
+        color: Colors.blue,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "te.",
+              style: TextStyle(
+                fontSize: 150
+              ),
+            ),
+            CircularProgressIndicator.adaptive()
+          ],
+        ),
+      ),
+    );
+  }
+}
